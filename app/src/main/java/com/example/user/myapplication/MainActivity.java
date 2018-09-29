3add1b55ddffef2cd5c5da2b05d300aa6375da00
audgk <audgk2025@naver.com>
2018-09-29 16:05:38 +0900

    image2

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.user.myapplication;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,20 +15,55 @@ import android.widget.ImageView;
 import net.alhazmy13.imagefilter.ImageFilter;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fgd);
+        context = this;
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.fgd);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bitmap bm = ImageFilter.applyFilter(bitmap, ImageFilter.Filter.SKETCH);
-                ImageView iv = findViewById(R.id.imageView);
-                iv.setImageBitmap(bm);
+                TransStatus transStatus = new TransStatus();
+                transStatus.execute();
             }
         });
+    }
+    class TransStatus extends AsyncTask<String, Void, String> {
+        ProgressDialog asyncDialog = new ProgressDialog(context);
+        @Override
+        protected String doInBackground(String... strings){
+            for(int i=0; i<5; i++){
+                asyncDialog.setProgress(i*30);
+                try{
+                    Thread.sleep(500);
+                }
+                catch (Exception e){
+                    Log.e("TAB", e.toString());
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            asyncDialog.dismiss();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            asyncDialog.setProgress(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("변환중입니다.");
+            Bitmap bm = ImageFilter.applyFilter(bitmap, ImageFilter.Filter.SKETCH);
+            ImageView iv = findViewById(R.id.imageView);
+            iv.setImageBitmap(bm);
+            asyncDialog.show();
+            super.onPreExecute();
+        }
     }
 }
